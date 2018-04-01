8aef01c459a300ead4c267c19caea9ce07251455
Omkar Vyas <ovyas@cpslab-linux2.qualcomm.com>
2018-04-01 00:28:35 -0700

    c++ stack_implementation basic program is added

--- L1/L2/c/os_concepts/small_concepts/c_basics/temp.c
+++ L1/L2/c/os_concepts/small_concepts/c_basics/temp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define B2(n) n , n+1, n+1, n+2
 #define B4(n) B2(n), B2(n+1), B2(n+1), B2(n+2)
@@ -10,17 +11,38 @@ char arr[256]={COUNT_BITS};
 
 int main(void){
 
-int i = 0;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int k = 0;
+unsigned int l = 0;
+unsigned int x = 0;
+unsigned int *rarr = NULL;
+unsigned int numb = 0;
 
-    for(i = 0; i <256; i++){
-    
-        printf(" arr[%d] = %d\t\t ",i, arr[i]);
-    
+
+    printf("\n input a positive number :");
+    scanf("%u",&numb);
+    printf("\n input  number :%u",numb);
+
+    rarr = (unsigned int *)calloc(numb+1,(sizeof(unsigned int)));
+    if(rarr == NULL){
+    printf("Could not allocate any memory");
+    return 0;
     }
+    rarr[0] = 0;
 
-//        printf("\n defination of B2 %s\n ", arr);
+        for(i = 1; i<=numb; ++i){
+            rarr[i] = rarr[(i&(i-1))]+1;
+        }
+    
+//    i = i-1;
+    
+    for(l = 0; l<i;l++){
+    printf("\n rarr[%u] = %u\n",l,rarr[l]);
+    }
+    
+    
 
-        
         return 0;
 
 
---
+++ L1/L2/cpp/stack/stack_basic.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+
+
+class Stack{
+
+    int stk_ptr;
+    int arr[MAX];
+
+        public:
+        Stack(){
+            stk_ptr = -1;
+    
+         }
+        void push(int data){
+        
+            cout<<"in push"<<endl;
+            stk_ptr++;
+            
+            cout<<"stk_ptr:"<<stk_ptr<<endl;
+            arr[stk_ptr] = data;
+        
+        }
+        int pop(){
+                   
+            cout<<"in pop"<<endl;
+            if(isempty()){
+            
+                cout<< "Stack is empty nothing can be popped"<<endl;
+                return -1;
+            
+            }
+            
+            stk_ptr--;
+
+        }
+        bool isempty(){
+                
+            cout<<"in isempty"<<endl;
+            if(stk_ptr == -1){
+                return true;
+            }
+            return false;
+        }
+        bool isfull(){
+                 
+            cout<<"in isfull"<<endl;
+            if(stk_ptr == MAX){
+                return true;
+                
+            }
+            return false;
+
+        
+        }
+        int top(){
+            int stack_top_val = 0;
+            cout<<"in top"<<endl;
+            cout<<"stk_ptr:"<<stk_ptr<<endl;
+            stack_top_val = arr[stk_ptr];
+            return stack_top_val;
+        }
+
+};
+
+int main(){
+    Stack mystack;
+
+    mystack.push(10);
+    mystack.push(20);
+    mystack.push(30);
+    mystack.push(40);
+    mystack.push(50);
+    int i = mystack.pop();
+    bool x = mystack.isempty();
+    bool y = mystack.isfull();    
+    int j = mystack.top();
+    cout<< "stack top: " <<j<<endl;
+    return 0;
+
+}
