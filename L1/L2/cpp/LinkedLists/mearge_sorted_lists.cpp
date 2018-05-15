1f8e08a0fe9b775a43a23c4fb15cba5737ccac49
Omkar Vyas <ovyas@cpslab-linux.qualcomm.com>
2018-05-15 10:53:43 -0700

    added some comments to understand the logic

--- L1/L2/cpp/LinkedLists/mearge_sorted_lists.cpp
+++ L1/L2/cpp/LinkedLists/mearge_sorted_lists.cpp
@@ -36,65 +36,43 @@ bool compare(int a, int b){
 
 ListNode * mearg_2_sorted_lists(ListNode *h1, ListNode *h2){
 
+
+/*create a dummy node*/
 ListNode *head = new ListNode(-1);
+/*create a pointer to be returend*/
 ListNode *ret_head = head;
-
-
 
     if(h1 == NULL){
         return h2;
-    
     }
 
     if(h2 == NULL){
         return h1;
-    
     }
 
     if(h1 == NULL & h2 == NULL){
         return NULL;
-    
     }
     
     while(h1!=NULL && h2!=NULL){
 
+        /*if first link current value is small than the second list current value assign head->next = h1 and h1 = h1->next;*/
         if(h1->val < h2->val){
-
             head->next = h1;
             h1 = h1->next;
     
-    
+        /*else if second link current value is small than the first list current value assign head->next = h2 and h2 = h2->next;*/
         }else{
-        
             head->next = h2;
             h2 = h2->next;
-        
         }
-
         head = head->next;
-
     }
 
     /*if expression 1 evaluates to true then expression 2 is evaluated*/
     /*if expression 1 evaluates to flase then expression 3 is evaluated*/
 
     head->next = h1?h1:h2;
-
-
-#if 0    
-    if(h1 == NULL && h2 == NULL){
-        head->next = NULL;
-    
-    }
-    if(h1 == NULL){
-    
-        head->next = h2;
-    }
-    if(h2 == NULL){
-    
-        head->next = h1;
-    }
-#endif    
     return ret_head->next;
 
 
@@ -136,7 +114,7 @@ int main(){
     head2 = AddNode_Front(head2,45);
     head2 = AddNode_Front(head2,35);
     head2 = AddNode_Front(head2,25);
-    head2 = AddNode_Front(head2,15);
+    head2 = AddNode_Front(head2,10);
 
     out_head = mearg_2_sorted_lists(head1,head2);
 
