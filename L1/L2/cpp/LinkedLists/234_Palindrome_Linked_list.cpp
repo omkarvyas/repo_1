54ec123848e3bbe68b5db500a60deb35f4cec6e0
Omkar Vyas <ovyas@cpslab-linux.qualcomm.com>
2018-05-16 11:46:35 -0700

    234 Linked List is Palindrome logic implemented

--- L1/L2/cpp/LinkedLists/234_Palindrome_Linked_list.cpp
+++ L1/L2/cpp/LinkedLists/234_Palindrome_Linked_list.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 
 using namespace std;
 
@@ -49,6 +50,91 @@ return tmp;
 }
 
 
+/*Given a singly linked list, determine if it is a palindrome.*/
+/*
+e.g.
+input Input: 1->2
+Output: false
+ 
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+ 
+ */
+
+
+
+bool isPalindrome(ListNode* head) {
+
+    /*traverse the whole list and find the half of it and push it on stack*/
+    /*when we reach to the half of the linked list start removing the elelemnts form the stack and compare with rest of the contents in the linked list*/
+
+    int count = 1;
+    ListNode *cur = head;
+    stack<ListNode*> my_stk;
+    bool odd_flag = 0;
+    int temp;
+    /*Find the length of the linked list*/
+    for (count = 1;cur->next!=NULL;++count){
+    
+        cur = cur->next;
+    
+    }
+
+    /*See if linked list is consist of odd number of nodes or even number of nodes set a odd flag as we want to remove the node which is at center of the lined list*/
+    cout <<"count = :"<<count<<endl;
+    if(count%2){
+    
+        cout<<"count is odd"<<endl;
+        count = count/2 + 1;
+        odd_flag = 1;
+    
+    }else{
+        cout<<"count is odd "<<endl;
+        count = count/2;
+    
+    }
+    cur = head;
+    while(count){
+    
+        if(cur!=NULL){
+            my_stk.push(cur);
+            cout<<"pushed on stack:    " <<cur->val;
+        }
+        cur = cur->next;
+        --count;
+    
+    }
+    /*remove the center node from the stack*/
+    if(odd_flag){
+        my_stk.pop();
+    
+    }
+    while(cur!=NULL){
+        cur = cur->next;
+    
+        if(my_stk.empty()!=true){
+            temp = (my_stk.top())->val;
+            cout<<"popped element from stack: "<<temp<<endl;
+            my_stk.pop();
+        }
+        if(cur!=NULL){
+            if(temp != cur->val){
+                return false;
+        
+            }
+        }
+        
+    }
+
+    return true;
+                 
+}
+
+
+
+
 
 
 
@@ -56,9 +142,18 @@ int main(){
 
     ListNode * head = NULL;
     int k = 4;
+    head = AddNode_Front(head,1);
+    head = AddNode_Front(head,2);
     head = AddNode_Front(head,2);
     head = AddNode_Front(head,1);
-    head = AddNode_Front(head,0);
+    if(isPalindrome(head)){
+    
+        cout<<"Linked list is not Palndrome"<<endl;
+    }else{
+    
+        cout<<"Linked list is Palndrome"<<endl;
+    
+    }
     Print_LL(head);
     return 0;
 }
