12a6ff363be094b60961621b6a39d0830fa255ad
Omkar Vyas <ovyas@cpslab-linux.qualcomm.com>
2018-09-04 23:03:39 -0700

    added files from Learn Bay to DSA_Learn_Bay directory

---
+++ L1/L2/cpp/tricks/function_on_left.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int& Return_ref(int &x){
+
+return (x);
+
+}
+
+
+int main(){
+
+int a = 10, b;
+b = Return_ref(a);
+
+cout<< "a = "<<a<<"and b = "<<b<<endl;
+
+Return_ref(a) = 3;
+
+
+cout<< "a = "<<a<<endl;
+return 0;
+
+}
+
+
+
+
